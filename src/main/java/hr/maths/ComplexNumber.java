19fd76ec24d9cb0fd1bea8d9c767f3475490841c
vilimstaroveski <vilimstaroveski@protonmail.com>
2020-09-14 18:59:48 +0200

    Add simple features.

--- src/main/java/hr/maths/ComplexNumber.java
+++ src/main/java/hr/maths/ComplexNumber.java
@@ -1,30 +1,78 @@
 package hr.maths;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ComplexNumber {
 
-	private double real;
-	private double imag;
+	private final double real;
+	private final double imag;
+	
+	private final static Pattern PATTERN_WHITESPACE = Pattern.compile("\\s*");
+	private final static Pattern PATTERN_COMPLEX_NUM = Pattern.compile("(-?\\d+|-?\\d+\\.\\d+)([\\+-]\\d+|[\\+-]\\d+\\.\\d+)i");
 	
 	public ComplexNumber() {
 		this.real = 0;
 		this.imag = 0;
 	}
-
-	public double getReal() {
-		return real;
+	
+	public ComplexNumber(double real, double imag) {
+		this.real = real;
+		this.imag = imag;
 	}
 
-	public void setReal(double real) {
-		this.real = real;
+	public static ComplexNumber parseComplexNumber(String str) throws NumberFormatException {
+		// step 1: remove ALL whitespaces
+		Matcher whitespaces = PATTERN_WHITESPACE.matcher(str);
+		String strWithoutSpaces = whitespaces.replaceAll("");
+		// step 2: match a (potential) complex number
+		Matcher m = PATTERN_COMPLEX_NUM.matcher(strWithoutSpaces);
+		if(!m.find())
+			// step 3a (if matching was unsuccessful): throw exception
+			throw new NumberFormatException();
+		// step 3b (if matching was successful): return new ComplexNumber
+		return new ComplexNumber(Double.parseDouble(m.group(1)), Double.parseDouble(m.group(2)));
+	}
+
+	public static ComplexNumber fromReal(double real) {
+		return new ComplexNumber(real, 0);
+	}
+	
+	public static ComplexNumber fromImaginary(double imag) {
+		return new ComplexNumber(0, imag);
+	}
+	
+	public double getReal() {
+		return real;
 	}
 
 	public double getImag() {
 		return imag;
 	}
-
-	public void setImag(double imag) {
-		this.imag = imag;
+	//not tested
+	public ComplexNumber add(ComplexNumber c) {
+		return new ComplexNumber(this.real + c.real, this.imag + c.imag);
+	}
+	//not tested
+	public ComplexNumber sub(ComplexNumber c) {
+		return new ComplexNumber(this.real - c.real, this.imag - c.imag);
+	}
+	//not tested
+	public ComplexNumber mul(ComplexNumber c) {
+		double realResult = this.real * c.real - this.imag * c.imag;
+		double imagResult = this.imag * c.real + this.real * c.imag;
+		return new ComplexNumber(realResult, imagResult);
 	}
 	
 	
+	public String toString() {
+		if(this.real == 0 && this.imag == 0)
+			return "0.0";
+		StringBuilder str = new StringBuilder("");
+		if(this.real != 0)
+			str.append(this.real);
+		if(this.imag != 0)
+			str.append((this.imag > 0) ? "+" + this.imag + "i" : this.imag + "i");
+		return str.toString();
+	}
 }
--- src/test/java/hr/maths/ComplexNumberTest.java
+++ src/test/java/hr/maths/ComplexNumberTest.java
@@ -16,4 +16,85 @@ public class ComplexNumberTest {
 		assertTrue(Math.abs(c.getReal() - expectedReal) < DOUBLE_EQUALS_THRESHOLD);
 		assertTrue(Math.abs(c.getImag() - expectedImag) < DOUBLE_EQUALS_THRESHOLD);
 	}
+	
+	@Test
+	public void complexNumberParsingIntValues() {
+		String[] strTest = {"1+2i", "1-2i", "-1+2i", "-1-2i"};
+		double[] expectedReal = {1.0,  1.0, -1.0, -1.0};
+		double[] expectedImag = {2.0, -2.0,  2.0, -2.0};
+		ComplexNumber c;
+		for(int i = 0; i < strTest.length; i++) {
+			c = ComplexNumber.parseComplexNumber(strTest[i]);
+			assertTrue(Math.abs(c.getReal() - expectedReal[i]) < DOUBLE_EQUALS_THRESHOLD);
+			assertTrue(Math.abs(c.getImag() - expectedImag[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	
+	@Test
+	public void complexNumberParsingWithWhiteSpaces() {
+		String[] strTest = {"	1+   12.3	i", "1.6   -	2i", " - 1.12 + 2.01 i	", "   -   111  -  2.35  i  "};
+		double[] expectedReal = { 1.0,  1.6, -1.12, -111.0};
+		double[] expectedImag = {12.3, -2.0,  2.01,   -2.35};
+		ComplexNumber c;
+		for(int i = 0; i < strTest.length; i++) {
+			c = ComplexNumber.parseComplexNumber(strTest[i]);
+			assertTrue(Math.abs(c.getReal() - expectedReal[i]) < DOUBLE_EQUALS_THRESHOLD);
+			assertTrue(Math.abs(c.getImag() - expectedImag[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	@Test
+	public void complexNumberParsingDoubleValues() {
+		String[] strTest = {"1.3+2.43i", "1.111-2.33i", "-0.01+2.2i", "-1.111-0.202i"};
+		double[] expectedReal = {1.3,   1.111, -0.01, -1.111};
+		double[] expectedImag = {2.43, -2.33,   2.2,  -0.202};
+		ComplexNumber c;
+		for(int i = 0; i < strTest.length; i++) {
+			c = ComplexNumber.parseComplexNumber(strTest[i]);
+			assertTrue(Math.abs(c.getReal() - expectedReal[i]) < DOUBLE_EQUALS_THRESHOLD);
+			assertTrue(Math.abs(c.getImag() - expectedImag[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	
+	@Test
+	public void complexNumberFromReal() {
+		double[] expectedReal = {2.43, -2.33,   2.2,  -0.202, 0, -1, 2};
+		ComplexNumber c;
+		for(int i = 0; i < expectedReal.length; i++) {
+			c = ComplexNumber.fromReal(expectedReal[i]);
+			assertTrue(Math.abs(c.getReal() - expectedReal[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	
+	@Test
+	public void complexNumberFromImag() {
+		double[] expectedImag = {2.43, -2.33,   2.2,  -0.202, 0, -1, 2};
+		ComplexNumber c;
+		for(int i = 0; i < expectedImag.length; i++) {
+			c = ComplexNumber.fromImaginary(expectedImag[i]);
+			assertTrue(Math.abs(c.getImag() - expectedImag[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	
+	@Test
+	public void complexNumberGettersTest() {
+		ComplexNumber[] testCases = {new ComplexNumber(), new ComplexNumber(0, 0), new ComplexNumber(1, 2), new ComplexNumber(-0.4, 3.2)};
+		double[] expectedReal = {0.0, 0.0, 1.0, -0.4};
+		double[] expectedImag = {0.0, 0.0, 2.0, 3.2};
+		for(int i = 0; i < testCases.length; i++) {
+			assertTrue(Math.abs(testCases[i].getReal() - expectedReal[i]) < DOUBLE_EQUALS_THRESHOLD);
+			assertTrue(Math.abs(testCases[i].getImag() - expectedImag[i]) < DOUBLE_EQUALS_THRESHOLD);
+		}
+	}
+	
+	@Test
+	public void complexNumberToStringTest() {
+		ComplexNumber[] testCases = {new ComplexNumber(), new ComplexNumber(0, 0), 
+									 new ComplexNumber(1, 2), new ComplexNumber(-0.4, 3.2),
+									 new ComplexNumber(0, -2.2), new ComplexNumber(1, 0)};
+		String[] expectedOutput = {"0.0", "0.0", "1.0+2.0i", "-0.4+3.2i", "-2.2i", "1.0"};
+		for(int i = 0; i < testCases.length; i++) {
+			assertTrue(testCases[i].toString().equals(expectedOutput[i]));
+		}
+	}
+	
 }
